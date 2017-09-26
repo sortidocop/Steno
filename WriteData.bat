932a7d977c7ddba2857dd475b1affddff7c5b856
sortidocop <sortidocorp@gmail.com>
2017-09-26 12:09:06 +0200

    Add files via upload

---
+++ WriteData.bat
@@ -0,0 +1,2 @@
+java -jar sortidocorpsteno.jar WRITE D:\workspace\steno\steno\data nothing
+pause
---
+++ showInfo.bat
@@ -0,0 +1,2 @@
+java -jar sortidocorpsteno.jar INFO nothing D:\steno\steno\data\DSC_2609.jpg 
+pause
